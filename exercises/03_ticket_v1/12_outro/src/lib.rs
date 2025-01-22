32c6932086b22c34a1393cbaf06ac16b579f3d59
brilu30 <brianlujan@outlook.com>
2025-01-21 21:39:57 -0500

    Chapter 3 completed

--- exercises/03_ticket_v1/12_outro/src/lib.rs
+++ exercises/03_ticket_v1/12_outro/src/lib.rs
@@ -11,3 +11,51 @@
 // Integration here has a very specific meaning: they test **the public API** of your project.
 // You'll need to pay attention to the visibility of your types and methods; integration
 // tests can't access private or `pub(crate)` items.
+pub struct Order{
+    product_name: String,
+    quantity: i32,
+    unit_price: i32
+}
+impl Order{
+    pub fn new(product_name: String, quantity: i32, unit_price: i32) -> Order {
+        Order { product_name, quantity, unit_price}
+    }
+    pub fn check_name(name: String){
+        if name.is_empty() || name.as_bytes().len() > 300{
+            panic!("No likey")
+        }
+    }
+    pub fn check_quantity(quantity: i32){
+        if quantity < 0{
+            panic!("Can't have none dude")
+        }
+    }
+    pub fn check_unit_price(unit_price: i32){
+        if unit_price < 0{
+            panic!("No free item :(")
+        }
+    }
+    pub fn total(&self) -> i32 {
+        self.unit_price * self.quantity
+    }
+
+    pub fn set_product_name(&mut self, name: String){
+        self.product_name = name;
+    }
+    pub fn set_quantity(&mut self, quantity: i32){
+        self.quantity = quantity;
+    }
+    pub fn set_unit_price(&mut self, unit_price: i32){
+        self.unit_price = unit_price;
+    }
+
+    pub fn get_product_name(&self) -> String {
+        self.product_name.clone()
+    }
+    pub fn get_quantity(&self) -> &i32 {
+        &self.quantity
+    }
+    pub fn get_unit_price(&self) -> &i32 {
+        &self.unit_price
+    }
+}
--- exercises/03_ticket_v1/12_outro/tests/integration.rs
+++ exercises/03_ticket_v1/12_outro/tests/integration.rs
@@ -8,18 +8,18 @@ use outro_02::Order;
 fn test_order() {
     let mut order = Order::new("Rusty Book".to_string(), 3, 2999);
 
-    assert_eq!(order.product_name(), "Rusty Book");
-    assert_eq!(order.quantity(), &3);
-    assert_eq!(order.unit_price(), &2999);
+    assert_eq!(order.get_product_name(), "Rusty Book");
+    assert_eq!(order.get_quantity(), &3);
+    assert_eq!(order.get_unit_price(), &2999);
     assert_eq!(order.total(), 8997);
 
     order.set_product_name("Rust Book".to_string());
     order.set_quantity(2);
     order.set_unit_price(3999);
 
-    assert_eq!(order.product_name(), "Rust Book");
-    assert_eq!(order.quantity(), &2);
-    assert_eq!(order.unit_price(), &3999);
+    assert_eq!(order.get_product_name(), "Rust Book");
+    assert_eq!(order.get_quantity(), &2);
+    assert_eq!(order.get_unit_price(), &3999);
     assert_eq!(order.total(), 7998);
 }
 
